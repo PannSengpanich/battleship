1f7106c253f85b54d9816f0df47206f7febe9b06
Pann Sengpanich <panncraftyo@gmail.com>
2023-05-18 16:44:54 +0700

    create files

---
+++ main.js
@@ -0,0 +1,13 @@
+function Ship() {
+  let length = 0;
+  let hits = 0;
+  let sink = false;
+  function hit() {}
+  function isSunk() {}
+}
+function Gameboard() {
+  let miss = [];
+  function receiveAttack(x, y) {}
+}
+
+function Player() {}
